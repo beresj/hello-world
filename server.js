613531f036dd8c5acce45d0294dd83e89fd2d027
beresj <beresj@supra.hu>
2017-06-01 14:12:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) { 
  res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-�res.end('Az elso Azure alapu Node oldalam \n');
+ res.end('Az első Azure alapu Node oldalam \n');
 }).listen(port);
